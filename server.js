4229af7fc451dd9faf71f8536c5bf543a7ccafc3
The Solo Artist <michael.solorzano@email.bakersfieldcollege.edu>
2020-09-22 14:58:28 -0700

    updated server.js

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js")(app);
-
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
 });
