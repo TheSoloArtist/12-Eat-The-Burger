104f939c04e96f1d5050ebdac360e44ab2e9a2ad
The Solo Artist <michael.solorzano@email.bakersfieldcollege.edu>
2020-09-22 14:37:45 -0700

    added git ignore and setup db connection

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,9 @@ var mysql = require("mysql");
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "burgers_db"
-    });
+    connection = mysql.createConnection(
+        process.env.JAWSDB_URI
+    );
 };
 
 connection.connect(function(err) {
